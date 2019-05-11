7316bd99f7fe078f5b663e3289de44192dac36e6
Trond Kandal <Trond.Kandal@ntnu.no>
2019-05-11 17:21:11 +0200

    Added a simple murmur3 checksum package.

---
+++ checksum.go
@@ -0,0 +1,29 @@
+package mm3cs
+
+import (
+	"bytes"
+	"encoding/hex"
+	"github.com/DataDog/mmh3"
+)
+
+/*
+ * Copyright (c) 2019 Norwegian University of Science and Technology
+ */
+
+// CheckSum implements some kind of checksum given a slice of bytes or string
+type CheckSum interface {
+	SumString(str string) string
+	SumBytes(b []byte) string
+}
+
+type Murmur3CheckSum struct {
+}
+
+func (mcs *Murmur3CheckSum) SumString(str string) string {
+	buf := bytes.NewBufferString(str)
+	return mcs.SumBytes(buf.Bytes())
+}
+
+func (mcs *Murmur3CheckSum) SumBytes(b []byte) string {
+	return hex.EncodeToString(mmh3.Hash128(b))
+}
